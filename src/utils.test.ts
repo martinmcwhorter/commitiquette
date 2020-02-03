ce0544e6ef3875a1dd06fc8772507febc3b9edc4
martin.mcwhorter <martin@mcwhorter.org>
2020-02-03 11:21:44 +0000

    feat: wordCase added

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,31 +1,47 @@
-import { getLongest, pipeWith } from "./utils"
+import { getLongest, pipeWith, wordCase } from './utils';
+import { Case } from '@commitlint/load';
 
 describe('getLongest', () => {
-
   test('return longest string lenth in array', () => {
+    const result = getLongest(['1', '333', '22']);
 
-    const result = getLongest(['1', '22'])
-
-    expect(result).toBe(2);
-  })
-
-})
+    expect(result).toBe(3);
+  });
+});
 
 describe('pipeWith', () => {
-
   test('pipes value to function', () => {
-
-    const result = pipeWith('hello ', x => x += 'world');
+    const result = pipeWith('hello ', x => (x += 'world'));
 
     expect(result).toBe('hello world');
-  })
+  });
 
   test('pipes to many functions', () => {
-
     const increment = (x: number) => ++x;
 
     const result = pipeWith(5, increment, increment, increment);
 
     expect(result).toBe(8);
-  })
-})
+  });
+});
+
+describe('wordCase', () => {
+  test.each<[string, Case, string]>([
+    ['FOO_BAR', 'lower-case', 'foo_bar'],
+    ['FOO_BAR', 'lowercase', 'foo_bar'],
+    ['FOO_BAR', 'lowerCase', 'foo_bar'],
+    ['fooBar', 'snake-case', 'foo_bar'],
+    ['fooBar', 'pascal-case', 'FooBar'],
+    ['fooBar', 'sentence-case', 'Foo bar'],
+    ['fooBar', 'sentencecase', 'Foo bar'],
+    ['fooBar', 'start-case', 'Foo Bar'],
+    ['fooBar', 'kebab-case', 'foo-bar'],
+    ['fooBar', 'upper-case', 'FOOBAR'],
+    ['fooBar', 'uppercase', 'FOOBAR'],
+    ['foo_Bar', 'camel-case', 'fooBar']
+  ])('wordCase(%s, %s): %s', (value, rule, expected) => {
+    const result = wordCase(value, rule);
+
+    expect(result).toBe(expected);
+  });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,41 @@
+import { Case } from '@commitlint/load';
+import { snakeCase, pascalCase, sentenceCase, capitalCase, paramCase, camelCase } from 'change-case';
+
 export const pipeWith = <T>(arg: T, ...fns: ((a: T) => T)[]) => fns.reduce((v, f) => f(v), arg);
 
 export function getLongest(array: string[]) {
   return array.reduce((x, y) => (x.length > y.length ? x : y)).length;
+}
+
+/* istanbul ignore next */
+export function assertNever(x: never): never {
+  throw new Error('Unexpected object ' + x);
+}
+
+export function wordCase(value: string, rule: Case): string {
+  switch (rule) {
+    case 'lower-case':
+    case 'lowerCase':
+    case 'lowercase':
+      return value.toLowerCase();
+    case 'snake-case':
+      return snakeCase(value);
+    case 'pascal-case':
+      return pascalCase(value);
+    case 'sentence-case':
+    case 'sentencecase':
+      return sentenceCase(value);
+    case 'start-case':
+      return capitalCase(value);
+    case 'kebab-case':
+      return paramCase(value);
+    case 'upper-case':
+    case 'uppercase':
+      return value.toUpperCase();
+    case 'camel-case':
+      return camelCase(value);
+    /* istanbul ignore next */
+    default:
+      return assertNever(rule);
+  }
 }
