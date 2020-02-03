4d420823adb5f0683a93218a49996ce1d36dde39
martin.mcwhorter <martin@mcwhorter.org>
2020-02-03 11:23:51 +0000

    feat: turn on/off prompts with when

---
+++ src/when.test.ts
@@ -0,0 +1,78 @@
+import { enumWhen, emptyWhen, whenFactory } from './when';
+import { Level, Rule } from '@commitlint/load';
+
+describe('when', () => {
+  describe('enumWhen', () => {
+    test('should return true if rule undefined', () => {
+      const result = enumWhen(undefined);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return true if level is disable', () => {
+      const result = enumWhen([Level.Disable, 'always', []]);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return true if applicable is never', () => {
+      const result = enumWhen([Level.Error, 'never', []]);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return true when array not empty', () => {
+      const result = enumWhen([Level.Error, 'always', ['foo']]);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return false when array empty', () => {
+      const result = enumWhen([Level.Error, 'always', []]);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('emptyWhen', () => {
+    test('should return true if rule undefined', () => {
+      const result = emptyWhen(undefined);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return true if level is disable', () => {
+      const result = emptyWhen([Level.Disable, 'always', undefined]);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return true if applicable is never', () => {
+      const result = emptyWhen([Level.Error, 'never', undefined]);
+
+      expect(result).toBe(true);
+    });
+
+    test('should return false if applicable is always', () => {
+      const result = emptyWhen([Level.Error, 'always', undefined]);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('whenFactory', () => {
+    const hidePromptEnumRule: Rule<string[]> | undefined = [Level.Error, 'always', []];
+    const hidePromptEmptyRule: Rule<undefined> | undefined = [Level.Error, 'always', undefined];
+
+    test.each<[Rule<string[]> | undefined, Rule<undefined> | undefined, boolean]>([
+      [undefined, undefined, true],
+      [undefined, hidePromptEmptyRule, false],
+      [hidePromptEnumRule, undefined, false],
+      [hidePromptEnumRule, hidePromptEmptyRule, false]
+    ])('%o, %o', (enumRule, emptyRule, expected) => {
+      const result = whenFactory(enumRule, emptyRule)();
+
+      expect(result).toBe(expected);
+    });
+  });
+});
---
+++ src/when.ts
@@ -0,0 +1,45 @@
+import { Rule, Level } from '@commitlint/load';
+
+export function enumWhen(rule: Rule<string[]> | undefined) {
+  if (rule == null) {
+    return true;
+  }
+
+  const [level, applicable, value] = rule;
+
+  if (level == Level.Disable) {
+    return true;
+  }
+
+  const emptyEnum = value.length == 0;
+
+  if (applicable == 'always') {
+    return !emptyEnum;
+  }
+
+  return true;
+}
+
+export function emptyWhen(rule: Rule<undefined> | undefined) {
+  if (rule == null) {
+    return true;
+  }
+
+  const [level, applicable] = rule;
+
+  if (level == Level.Disable) {
+    return true;
+  }
+
+  return applicable == 'never';
+}
+
+export function whenFactory(
+  enumRule: Rule<string[]> | undefined,
+  emptyRule: Rule<undefined> | undefined
+): () => boolean {
+  return () => {
+    // return false if either of the rules return false
+    return ![enumWhen(enumRule), emptyWhen(emptyRule)].includes(false);
+  };
+}
