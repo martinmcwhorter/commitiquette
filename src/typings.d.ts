648ccd92fa198b634d4cd2c6246006d163aa351b
martin.mcwhorter <martin@mcwhorter.org>
2020-02-03 11:16:27 +0000

    improvement: cleanup typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -7,12 +7,17 @@ declare module '@commitlint/load' {
 
   export type Case =
     | 'lower-case'
+    | 'lowercase'
+    | 'lowerCase'
     | 'upper-case'
+    | 'uppercase'
     | 'camel-case'
     | 'kebab-case'
     | 'pascal-case'
     | 'sentence-case'
-    | 'start-case';
+    | 'sentencecase'
+    | 'start-case'
+    | 'snake-case';
 
   export type Applicability = 'always' | 'never';
 
@@ -23,27 +28,27 @@ declare module '@commitlint/load' {
     'body-max-length'?: Rule<number>;
     'body-max-line-number'?: Rule<number>;
     'body-min-length'?: Rule<number>;
-    'footer-leading-blank'?: Rule<void>;
+    'footer-leading-blank'?: Rule<undefined>;
     'footer-max-length'?: Rule<number>;
     'footer-min-length'?: Rule<number>;
-    'header-case'?: Rule<Case | Case[]>;
+    'header-case'?: Rule<Case>;
     'header-full-stop'?: Rule<string>;
     'header-max-length'?: Rule<number>;
     'header-min-length'?: Rule<number>;
-    'references-empty'?: Rule<void>;
+    'references-empty'?: Rule<undefined>;
     'scope-enum'?: Rule<string[]>;
-    'scope-case'?: Rule<Case | Case[]>;
-    'scope-empty'?: Rule<void>;
+    'scope-case'?: Rule<Case>;
+    'scope-empty'?: Rule<undefined>;
     'scope-max-length'?: Rule<number>;
     'scope-min-length'?: Rule<number>;
     'subject-case'?: Rule<Case>;
-    'subject-empty'?: Rule<void>;
+    'subject-empty'?: Rule<undefined>;
     'subject-full-stop'?: Rule<string>;
     'subject-max-length'?: Rule<number>;
     'subject-min-length'?: Rule<number>;
     'type-enum'?: Rule<string[]>;
-    'type-case'?: Rule<Case | Case[]>;
-    'type-empty'?: Rule<void>;
+    'type-case'?: Rule<Case>;
+    'type-empty'?: Rule<undefined>;
     'type-max-length'?: Rule<number>;
     'type-min-length'?: Rule<number>;
     'signed-off-by'?: Rule<string>;
@@ -68,35 +73,6 @@ declare module 'commitizen' {
 
   export type Commit = (value: string) => void;
 }
-
-// declare module 'inquirer' {
-
-//   export type Choice = {
-//     name: string,
-//     value: string ,
-//     short?: string
-//   } | string | number;
-
-//   export type Value = string | number | boolean | string[] | number[] | null;
-//   export type Answers = Record<string, Value>;
-
-//   export type Question<T extends Answers = {}, U extends Value = any> = {
-//     type?: 'input' | 'number' | 'confirm' | 'list' | 'rawlist' | 'expand' | 'checkbox' | 'password' | 'editor',
-//     name: string,
-//     message?: ((answers: T) => boolean) | string,
-//     default?: Value | ((answers: T) => Value),
-//     choices?: number[] | string[] | Choice[],
-//     validate?: (value: U, answers: any) => boolean | string,
-//     filter?: (value: U) => U,
-//     transformer?: (value: U, answers: T) => U,
-//     when?: (answers: any) => boolean
-//   };
-
-//   export type Prompt = <T extends Answers = {}>(questions: Question<T>[]) => Promise<T>;
-//   export let prompt: Prompt;
-
-//   export type Commit = (value: string) => void;
-// }
 
 declare module 'conventional-commit-types' {
   export type CommitType = Record<string, { title: string; description: string }>;
