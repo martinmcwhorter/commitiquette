4bb2545e658e51998465c2405db9631aa526fe13
martin.mcwhorter <martin@mcwhorter.org>
2020-02-04 22:37:10 +0000

    build: fix lint issue

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import commitlintLoad from '@commitlint/load';
-import { engine } from './engine';
-import { PromptModule } from 'inquirer';
+import { PromptModule, prompt } from 'inquirer';
 import { Commit } from 'commitizen';
+import { engine } from './engine';
 
 export async function prompter(cz: { prompt: PromptModule }, commit: Commit) {
   const clConfig = await commitlintLoad();
   engine(clConfig, cz.prompt, commit);
 }
+
+// prompter({ prompt }, value => console.log(value));
