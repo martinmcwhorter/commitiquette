b680b159fa3ec4ab1e6d299b325ea666db71b7ed
mehboodian <xeptore@gmail.com>
2021-03-31 22:49:12 +0430

    refactor(validators): adapt with dependencies upgrade

--- src/validators.ts
+++ src/validators.ts
@@ -1,39 +1,40 @@
-import { Applicability, Case, Level, Rule } from '@commitlint/load';
+import type { TargetCaseType, RuleConfigTuple, RuleConfigCondition } from '@commitlint/types';
+import { RuleConfigSeverity } from '@commitlint/types';
 import { wordCase } from './utils';
 
 type ValidateRulesWithRuleValue = (value: string, ruleValue: number) => boolean;
-type ValidateRulesWithCaseValue = (value: string, ruleValue: Case, inclusive?: boolean) => boolean;
+type ValidateRulesWithCaseValue = (value: string, ruleValue: TargetCaseType, inclusive?: boolean) => boolean;
 type ValidatorRulesWithoutValue = (value: string) => boolean;
 type Validator = ValidateRulesWithRuleValue | ValidatorRulesWithoutValue | ValidateRulesWithCaseValue;
 
 export function validate(
   validators: {
     value: string;
-    rule: Rule<number | Case | Case[] | undefined> | undefined;
+    rule: RuleConfigTuple<number | TargetCaseType | TargetCaseType[] | undefined> | undefined;
     validator: Validator;
-    message: (length?: number | Case | Case[], applicable?: Applicability) => string;
+    message: (length?: number | TargetCaseType | TargetCaseType[], applicable?: RuleConfigCondition) => string;
   }[]
 ): string | true {
   const errorMessages: string[] = validators
     .map((v) => {
-      if (v.rule == undefined) {
+      if (v.rule === undefined) {
         return true;
       }
 
       const [level, applicable, ruleValue] = v.rule;
 
-      if (level !== Level.Error) {
+      if (level !== RuleConfigSeverity.Error) {
         return true;
       }
 
       let valid: boolean;
-      if (Array.isArray(ruleValue) && applicable == 'never') {
+      if (Array.isArray(ruleValue) && applicable === 'never') {
         valid = v.validator(v.value, ruleValue as never, false);
       } else {
         valid = v.validator(v.value, ruleValue as never);
       }
 
-      if (applicable == 'never') {
+      if (applicable === 'never') {
         valid = !valid;
       }
 
@@ -68,14 +69,14 @@ export function emptyValidator(value: string): boolean {
   return value.length < 1;
 }
 
-export function caseValidator(value: string, rule: Case | Case[], inclusive = true): boolean {
+export function caseValidator(value: string, rule: TargetCaseType | TargetCaseType[], inclusive = true): boolean {
   if (typeof rule === 'string') {
     return value == wordCase(value, rule);
   }
 
   if (inclusive) {
-    return rule.every((r) => wordCase(value, r) == value);
+    return rule.every((r) => wordCase(value, r) === value);
   }
 
-  return rule.some((r) => wordCase(value, r) == value);
+  return rule.some((r) => wordCase(value, r) === value);
 }
