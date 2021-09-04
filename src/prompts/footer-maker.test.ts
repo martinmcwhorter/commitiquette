5d54b7ae3b85ced11d589bce2f5a52baba702277
mbehboodian <29199390+xeptore@users.noreply.github.com>
2021-09-04 20:44:55 +0430

    test: add footer-maker issueFilterFactory test

--- src/prompts/footer-maker.test.ts
+++ src/prompts/footer-maker.test.ts
@@ -1,5 +1,5 @@
 import { QualifiedRules, RuleConfigSeverity } from '@commitlint/types';
-import { breakingChangeFilterFactory } from './footer-maker';
+import { breakingChangeFilterFactory, issueFilterFactory } from './footer-maker';
 
 describe('footerMaker', () => {
   describe('breakingChangeFilterFactory', () => {
@@ -10,6 +10,17 @@ describe('footerMaker', () => {
       const result = fixture('Lorem ipsum dolor sit amet, consectetur adipisicing elit.', {});
 
       expect(result).toBe(`BREAKING \nCHANGE: \nLorem ipsum \ndolor sit \namet, \nconsectetur \nadipisicing \nelit.`);
+    });
+  });
+
+  describe('issueFilterFactory', () => {
+    it('should add leading blank line before issue line if is breaking change', () => {
+      const rules = {};
+
+      const fixture = issueFilterFactory(rules)!;
+
+      const result = fixture('Closes #123', { isBreaking: true });
+      expect(result).toBe('\nCloses #123');
     });
   });
 });
